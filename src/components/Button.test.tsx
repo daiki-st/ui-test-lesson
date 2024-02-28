114b8a22b977a9db41bac6d21a1d06a2e35fe963
Daiki Sato <daiki.sato.03@gmail.com>
2024-02-28 22:49:15 +0900

    29.React Testing Libraryの基礎

---
+++ src/components/Button.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Button", () => {
+  it("buttonタグがレンダリングされる", () => {
+    render(<Button label="ボタン" onClick={() => alert("click")}></Button>);
+
+    const element = screen.getAllByRole("button");
+    console.log(element);
+    expect(element[0]).toBeInTheDocument();
+    expect(element[0]).toHaveTextContent("ボタン");
+  });
+});
